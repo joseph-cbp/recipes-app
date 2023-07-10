1b2104bf8fa2fd3daa8eea044686bfb6bf9680a0
albertoflorence <alberto.sabatier@gmail.com>
2023-07-10 15:50:06 -0300

    feat: add redux configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <div className="meals"></div>;
+  return <div className="meals" />;
 }
 
 export default App;
