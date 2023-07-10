86376d59accb68b19e267a307b8541cef6a73a30
jp-almada <joaopedroaa200121@gmail.com>
2023-07-10 19:12:33 -0300

    fix: coverage 90%

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,73 +1,106 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Login from '../pages/Login';
 
 describe('Componente de Login', () => {
-  test('renderiza o formulário de login', () => {
-    const { getByTestId } = render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>,
-    );
-    const form = getByTestId('content-login');
-    expect(form).toBeInTheDocument();
+  it('renderiza os campos de email e senha', () => {
+    render(<Login />, { wrapper: MemoryRouter });
+
+    expect(screen.getByTestId('email-input')).toBeInTheDocument();
+    expect(screen.getByTestId('password-input')).toBeInTheDocument();
   });
 
-  test('atualiza corretamente os campos de e-mail e senha', () => {
-    const { getByTestId } = render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>,
-    );
-    const emailInput = getByTestId('email-input');
-    const passwordInput = getByTestId('password-input');
+  it('renderiza o botão de login', () => {
+    render(<Login />, { wrapper: MemoryRouter });
 
+    expect(screen.getByTestId('login-submit-btn')).toBeInTheDocument();
+  });
+
+  it('atualiza o valor do campo de email', () => {
+    render(<Login />, { wrapper: MemoryRouter });
+
+    const emailInput = screen.getByTestId('email-input');
     fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: '123456' } });
 
     expect(emailInput.value).toBe('test@example.com');
-    expect(passwordInput.value).toBe('123456');
   });
 
-  test('desabilita o botão de envio quando o comprimento da senha é menor ou igual a 6 caracteres', () => {
-    const { getByTestId } = render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>,
-    );
-    const passwordInput = getByTestId('password-input');
-    const submitButton = getByTestId('login-submit-btn');
+  it('atualiza o valor do campo de senha', () => {
+    render(<Login />, { wrapper: MemoryRouter });
 
-    fireEvent.change(passwordInput, { target: { value: '123456' } });
-    expect(submitButton.disabled).toBe(true);
+    const passwordInput = screen.getByTestId('password-input');
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+    expect(passwordInput.value).toBe('password123');
   });
 
-  test('desabilita o botão de envio quando o e-mail é inválido', () => {
-    const { getByTestId } = render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>,
-    );
-    const emailInput = getByTestId('email-input');
-    const submitButton = getByTestId('login-submit-btn');
+  it('desabilita o botão de login quando o email é inválido', () => {
+    render(<Login />, { wrapper: MemoryRouter });
 
-    fireEvent.change(emailInput, { target: { value: 'invalid-email@example.com' } });
-    expect(submitButton.disabled).toBe(true);
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const loginButton = screen.getByTestId('login-submit-btn');
+
+    fireEvent.change(emailInput, { target: { value: 'invalid-email' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+    expect(loginButton).toBeDisabled();
   });
 
-  test('chama a função handleSubmit quando o formulário é enviado', () => {
-    const { getByTestId } = render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>,
-    );
-    const form = getByTestId('content-login');
-    const handleSubmit = jest.fn();
+  it('desabilita o botão de login quando a senha é inválida', () => {
+    render(<Login />, { wrapper: MemoryRouter });
 
-    form.addEventListener('submit', handleSubmit);
-    fireEvent.submit(form);
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const loginButton = screen.getByTestId('login-submit-btn');
 
-    expect(handleSubmit).toHaveBeenCalledTimes(1);
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'pass' } });
+
+    expect(loginButton).toBeDisabled();
   });
+
+  it('habilita o botão de login quando o email e a senha são válidos', () => {
+    render(<Login />, { wrapper: MemoryRouter });
+
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const loginButton = screen.getByTestId('login-submit-btn');
+
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+    expect(loginButton).toBeEnabled();
+  });
+
+  it('salva o email do usuário no localStorage após o envio do formulário', () => {
+    render(<Login />, { wrapper: MemoryRouter });
+
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const loginButton = screen.getByTestId('login-submit-btn');
+
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    fireEvent.click(loginButton);
+
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    expect(user).toEqual({ email: 'test@example.com' });
+  });
+
+  it('redireciona o usuário para a página de refeições após o login bem-sucedido', () => {
+    render(<Login />);
+  
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const loginButton = screen.getByTestId('login-submit-btn');
+  
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    fireEvent.click(loginButton);
+  
+    expect(window.location.pathname).toBe('/meals');
+  });  
 });
