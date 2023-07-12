1a869dd45f5c77d2904d2a9e7cc931b57bc56bc7
albertoflorence <alberto.sabatier@gmail.com>
2023-07-12 14:03:50 -0300

    feat: requisito 38 e 39 - página recipe-progress

--- src/pages/RecipeProgress/index.jsx
+++ src/pages/RecipeProgress/index.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { fetchRecipeById } from '../../services';
+import './RecipeProgress.css';
+
+const getIngredients = (recipe) => Object.entries(recipe)
+  .filter(([key, value]) => key.includes('Ingredient') && value)
+  .map(([, value]) => value);
 
 const mapProperties = (recipe) => {
   const { strMealThumb,
     strDrinkThumb,
-    strMeal, strDrink,
+    strMeal,
+    strDrink,
     strCategory,
     strInstructions } = recipe;
 
@@ -14,6 +20,7 @@ const mapProperties = (recipe) => {
     title: strMeal || strDrink,
     category: strCategory,
     instructions: strInstructions,
+    ingredients: getIngredients(recipe),
   };
 };
 
@@ -23,22 +30,41 @@ export default function RecipeProgress() {
 
   useEffect(() => {
     const fetchRecipe = async () => {
-      const [recipeType, recipeId] = location.pathname.split('/').slice(1);
+      const [, recipeType, recipeId] = location.pathname.split('/');
       const result = await fetchRecipeById(recipeId, recipeType);
       setRecipe(mapProperties(result));
     };
     fetchRecipe();
   }, [location]);
-  const { image, title, category, instructions } = recipe;
+  const { image, title, category, instructions, ingredients } = recipe;
   return (
     <div>
-      <img data-testid="recipe-photo" src={ image } alt={ title } />
-      <h4 data-testid="recipe-title">{title}</h4>
-      <button data-testid="share-btn">Share</button>
-      <button data-testid="favorite-btn">Favorite</button>
-      <span data-testid="recipe-category">{category}</span>
-      <p data-testid="instructions">{instructions}</p>
-      <button data-testid="finish-recipe-btn">Finish recipe</button>
+      <header className="progress-header">
+        <img data-testid="recipe-photo" src={ image } alt={ title } />
+        <span data-testid="recipe-category">{category}</span>
+        <span data-testid="recipe-category">{category}</span>
+        <button data-testid="share-btn">Share</button>
+        <button data-testid="favorite-btn">Favorite</button>
+      </header>
+      <div className="progress-content">
+        <div className="progress-ingredients">
+          <h3>Ingredients</h3>
+          <ul className="progress-ingredients">
+            {ingredients && ingredients.map((ingredient, index) => (
+              <li key={ index } data-testid={ `${index}-ingredient-step` } className="">
+                <label>
+                  <input type="checkbox" className="ingredient-input" />
+                  {ingredient}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <h3 data-testid="recipe-title">{title}</h3>
+        <p data-testid="instructions" className="progress-instructions">{instructions}</p>
+        <button data-testid="finish-recipe-btn">Finish recipe</button>
+      </div>
     </div>
   );
 }
