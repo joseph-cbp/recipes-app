4f95bd9154c435fc4b6c877d56adde92b5e2e096
albertoflorence <alberto.sabatier@gmail.com>
2023-07-13 11:13:30 -0300

    refactor: fetchRecipeById

--- src/services/index.js
+++ src/services/index.js
@@ -8,20 +8,18 @@ export const fetchSearch = async (search, type, recipeType) => {
     firstLetter: 'search.php?f=',
     categories: 'list.php?c=list',
     categoryFilter: 'filter.php?c=',
+    id: 'lookup.php?i=',
   };
   const url = `${baseURL}${types[type]}${search}`;
   const result = await fetch(url).then((r) => r.json());
   return result[recipeType];
 };
 
-export const fetchMealOrDrink = async (type) => fetchSearch('', 'name', type);
+export const fetchMealOrDrink = async (recipeType) => fetchSearch('', 'name', recipeType);
 
 export const fetchRecipeById = async (id, recipeType) => {
-  const urlMeals = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
-  const urlDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
-  const url = recipeType === 'meals' ? urlMeals : urlDrinks;
-  const result = await fetch(`${url}${id}`).then((r) => r.json());
-  return result[recipeType][0];
+  const result = await fetchSearch(id, 'id', recipeType);
+  return result[0];
 };
 
 export const fetchCategories = async (type) => fetchSearch('', 'categories', type);
